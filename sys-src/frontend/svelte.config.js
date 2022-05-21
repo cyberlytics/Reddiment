f974bc96566e2082e1f4093aee09c1c3bb02f5e7
Stangl, Philipp <p.stangl1@oth-aw.de>
2022-05-21 13:11:53 +0200

    Add tailwindcss

--- sys-src/frontend/svelte.config.js
+++ sys-src/frontend/svelte.config.js
@@ -3,10 +3,12 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
+	// https://github.com/sveltejs/svelte-preprocess
+    preprocess: [
+        preprocess({
+            postcss: true,
+        }),
+    ],
 	kit: {
 		adapter: adapter()
 	}
