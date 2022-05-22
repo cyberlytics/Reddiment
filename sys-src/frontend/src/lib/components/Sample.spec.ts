e3fb2d5311119a7cc17635b6ece4c037fb9e559b
Stangl, Philipp <p.stangl1@oth-aw.de>
2022-05-22 07:50:41 +0200

    Add sample component test

---
+++ sys-src/frontend/src/lib/components/Sample.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest';
+import { render } from '@testing-library/svelte';
+import Sample from './Sample.svelte';
+
+describe("Sample Test", () => {
+
+    it('should not throw an error', () => {
+        const { getByText } = render(Sample);
+        expect(() => getByText(/Sample/i)).not.toThrow();
+    })
+})
