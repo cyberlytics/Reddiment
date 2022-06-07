9c9c5b0cbf29a6b6638ecbc259505490872d8eac
Holl, Daniel <d.holl1@oth-aw.de>
2022-06-07 09:21:58 +0200

    backend-database connection -> Type  definitones

--- sys-src/backend/src/services/database.mock.ts
+++ sys-src/backend/src/services/database.mock.ts
@@ -1,21 +1,16 @@
 import { SimpleCache } from "../util/cache";
 import { distinct } from "../util/list";
 import date from "../util/time";
+import { DbComment, IDatabase } from "./database";
 
 
-type DbComment = {
-    subreddit: string,
-    text: string,
-    timestamp: Date,
-    sentiment: number,
-};
 
-type DbComments = Array<DbComment>;
+type Comments = Array<DbComment>;
 
 /**
  * A simple and very basic mock for Elasticsearch database
  */
-class DbMock {
+class DbMock implements IDatabase {
     private readonly cache: SimpleCache;
 
     constructor() {
@@ -23,19 +18,29 @@ class DbMock {
         this.cache.set("comments", new Array<DbComment>());
     }
 
-    private comments(): DbComments {
-        return this.cache.get<DbComments>("comments")!;
+    private comments(): Comments {
+        return this.cache.get<Comments>("comments")!;
     }
 
-    public addComment(subreddit: string, text: string, timestamp: Date, sentiment: number): void {
-        this.comments().push({ subreddit, text, timestamp, sentiment });
+    public addComment(comment: DbComment): boolean {
+        this.comments().push(comment);
+        return true;
+    }
+
+    private addCommentRaw(subreddit: string, text: string, timestamp: Date, sentiment: number): void {
+        this.addComment({
+            sentiment: sentiment,
+            subreddit: subreddit,
+            text: text,
+            timestamp: timestamp,
+        });
     }
 
     public getSentiments(subreddit: string, from: Date, to: Date, keywords: Array<string>): Array<{ time: Date, sentiment: number }> {
         const commentsOfSubreddit = this.comments().filter(c => c.subreddit == subreddit);
         const filtered = commentsOfSubreddit.filter(c => c.timestamp >= from &&
-                                                         c.timestamp <= to &&
-                                                         (keywords.length == 0 || keywords.some(kw => c.text.includes(kw))));
+            c.timestamp <= to &&
+            (keywords.length == 0 || keywords.some(kw => c.text.includes(kw))));
         return filtered.map(c => { return { time: c.timestamp, sentiment: c.sentiment }; });
     }
 
@@ -44,23 +49,23 @@ class DbMock {
     }
 
     public initDummy(): void {
-        this.addComment("r/wallstreetbets", "the brown fox jumps over the lazy dog", date("2022-05-27Z"), 0.76);
-        this.addComment("r/wallstreetbets", "the red fox jumps over the lazy dog", date("2022-05-27Z"), 0.34);
-        this.addComment("r/wallstreetbets", "the green fox jumps over the lazy dog", date("2022-05-27Z"), 0.12);
-        this.addComment("r/wallstreetbets", "the blue fox jumps over the lazy dog", date("2022-05-27Z"), 0.50);
-        this.addComment("r/wallstreetbets", "the black fox jumps over the lazy dog", date("2022-05-27Z"), 0.64);
+        this.addCommentRaw("r/wallstreetbets", "the brown fox jumps over the lazy dog", date("2022-05-27Z"), 0.76);
+        this.addCommentRaw("r/wallstreetbets", "the red fox jumps over the lazy dog", date("2022-05-27Z"), 0.34);
+        this.addCommentRaw("r/wallstreetbets", "the green fox jumps over the lazy dog", date("2022-05-27Z"), 0.12);
+        this.addCommentRaw("r/wallstreetbets", "the blue fox jumps over the lazy dog", date("2022-05-27Z"), 0.50);
+        this.addCommentRaw("r/wallstreetbets", "the black fox jumps over the lazy dog", date("2022-05-27Z"), 0.64);
 
-        this.addComment("r/wallstreetbets", "the brown cat jumps over the lazy dog", date("2022-05-28Z"), 0.57);
-        this.addComment("r/wallstreetbets", "the red dog jumps over the lazy dog", date("2022-05-28Z"), 0.64);
-        this.addComment("r/wallstreetbets", "the green horse jumps over the lazy dog", date("2022-05-28Z"), 0.06);
-        this.addComment("r/wallstreetbets", "the blue pig jumps over the lazy dog", date("2022-05-28Z"), 0.24);
-        this.addComment("r/wallstreetbets", "the black chicken jumps over the lazy dog", date("2022-05-28Z"), 0.79);
+        this.addCommentRaw("r/wallstreetbets", "the brown cat jumps over the lazy dog", date("2022-05-28Z"), 0.57);
+        this.addCommentRaw("r/wallstreetbets", "the red dog jumps over the lazy dog", date("2022-05-28Z"), 0.64);
+        this.addCommentRaw("r/wallstreetbets", "the green horse jumps over the lazy dog", date("2022-05-28Z"), 0.06);
+        this.addCommentRaw("r/wallstreetbets", "the blue pig jumps over the lazy dog", date("2022-05-28Z"), 0.24);
+        this.addCommentRaw("r/wallstreetbets", "the black chicken jumps over the lazy dog", date("2022-05-28Z"), 0.79);
 
-        this.addComment("r/place", "the brown fox jumps over the fat dog", date("2022-05-27Z"), 0.31);
-        this.addComment("r/place", "the red fox jumps over the lonely dog", date("2022-05-27Z"), 0.72);
-        this.addComment("r/place", "the green fox jumps over the big dog", date("2022-05-28Z"), 0.87);
-        this.addComment("r/place", "the blue fox jumps over the scared dog", date("2022-05-28Z"), 0.38);
-        this.addComment("r/place", "the black fox jumps over the small dog", date("2022-05-28Z"), 0.04);
+        this.addCommentRaw("r/place", "the brown fox jumps over the fat dog", date("2022-05-27Z"), 0.31);
+        this.addCommentRaw("r/place", "the red fox jumps over the lonely dog", date("2022-05-27Z"), 0.72);
+        this.addCommentRaw("r/place", "the green fox jumps over the big dog", date("2022-05-28Z"), 0.87);
+        this.addCommentRaw("r/place", "the blue fox jumps over the scared dog", date("2022-05-28Z"), 0.38);
+        this.addCommentRaw("r/place", "the black fox jumps over the small dog", date("2022-05-28Z"), 0.04);
     }
 }
 
