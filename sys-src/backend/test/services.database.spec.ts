fbac0b71693c6a111fedf2255a7daeb7bf5ed71a
Holl, Daniel <d.holl1@oth-aw.de>
2022-06-07 13:14:50 +0200

    Create Class ElasticDb with function addComment() and test

---
+++ sys-src/backend/test/services.database.spec.ts
@@ -0,0 +1,10 @@
+import assert from "assert";
+import { ElasticDb } from "../src/services/database";
+
+describe("Elastic Database", () => {
+    it("should return true on inserting a comment", async () => {
+        const db = new ElasticDb();
+        const result = await db.addComment({ subreddit: 'r/test', sentiment: 1, text: 'Hallo', timestamp: new Date() });
+        assert.deepStrictEqual(result, true);
+    });
+});
