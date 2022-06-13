b1bb277ae061c94ecfc5e2a7081e966ef69b3e78
Weigl, Wolfgang <w.weigl@oth-aw.de>
2022-06-13 12:47:27 +0200

    strukturelle Änderungen init Dockerfile

---
+++ sys-src/reddit-crawler/build/snoowrap/snoowrap.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.requester = void 0;
+const snoowrap_1 = __importDefault(require("snoowrap"));
+exports.requester = new snoowrap_1.default({
+    username: 'reddiment-bot',
+    password: 'reddiment123,.-',
+    userAgent: 'reddiment-script',
+    clientId: '8OguKkRA6T2BhvMi0C-bcA',
+    clientSecret: 'pijvpzZ0rCkrGTegt4ndliPJjHkcKw'
+});
+//# sourceMappingURL=snoowrap.js.map
--- sys-src/reddit-crawler/src/snoowrap/snoowrap.js
+++ sys-src/reddit-crawler/src/snoowrap/snoowrap.js
@@ -1,9 +1,11 @@
-import Snoowrap from 'snoowrap'
-
-export const requester = new Snoowrap({
+"use strict";
+exports.__esModule = true;
+exports.requester = void 0;
+var Snoowrap = require("snoowrap");
+exports.requester = new Snoowrap({
     username: 'reddiment-bot',
     password: 'reddiment123,.-',
     userAgent: 'reddiment-script',
     clientId: '8OguKkRA6T2BhvMi0C-bcA',
     clientSecret: 'pijvpzZ0rCkrGTegt4ndliPJjHkcKw'
-})
+});
