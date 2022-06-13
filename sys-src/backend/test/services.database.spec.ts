98a3bd2e92072b72f51302eae26e5725901e3ded
Imeraj, Ardian <a.imeraj@oth-aw.de>
2022-06-13 10:49:53 +0200

    #10 Implement test for Elastic-Database

--- sys-src/backend/test/services.database.spec.ts
+++ sys-src/backend/test/services.database.spec.ts
@@ -4,7 +4,7 @@ import { ElasticDb } from "../src/services/database";
 describe("Elastic Database", () => {
     it("should return true on inserting a comment", async () => {
         const db = new ElasticDb(s => null);
-        const result = await db.addComment({ subreddit: 'r/test', sentiment: 1, text: 'Hallo', timestamp: new Date() });
+        const result = await db.addComment({ subreddit: 'r/wallstreetbets', sentiment: 1, text: 'the brown fox jumps over the lazy dog', timestamp: new Date(Date.UTC(2022,5,13,10,30,0,0)) });
         assert.deepStrictEqual(result, true);
     });
 
@@ -13,4 +13,17 @@ describe("Elastic Database", () => {
         const result = await db.pingElastic();
         assert.deepStrictEqual(result,true);
     });
+
+    it("Should retrun Array with Strings", async () => {
+        const db = new ElasticDb (s => null);
+        const result = await db.getSubreddits();
+        assert.deepStrictEqual(result, [ "r/wallstreetbets"])
+    })
+
+    it("Should retrun Array with TimeSentiments", async () => {
+        const db = new ElasticDb (s => null);
+        const result = await db.getSentiments('r/wallstreetbets', new Date(Date.UTC(2022,5,13,10,0,0,0)), new Date(Date.UTC(2022,5,13,11,0,0,0)), ['fox']);
+        assert.deepStrictEqual(result, [{time:'2022-06-13T10:30:00.000Z',sentiment:1}])
+    })
+
 });
