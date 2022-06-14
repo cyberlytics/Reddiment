7357ec6961b7062a897c01b82b9094bcdfcb1132
Stangl, Philipp <p.stangl1@oth-aw.de>
2022-06-14 19:57:58 +0200

    #12 Add footer and navbar component to layout

--- sys-src/frontend/src/routes/__layout.svelte
+++ sys-src/frontend/src/routes/__layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
     import "../app.css";
+    import "flowbite/dist/flowbite.css";
+    import Navbar from "$components/Navbar.svelte";
+    import Footer from "$components/Footer.svelte";
     import { KQL__Init } from '$lib/graphql/_kitql/graphqlStores'
     KQL__Init()
 </script>
 
 <div class="flex flex-col h-screen">
-    <header class="p-4 bg-gray-400">Reddiment</header>
+    <Navbar />
     <slot/>
-    <footer class="p-4 bg-gray-400">Footer</footer>
+    <Footer />
 </div>
