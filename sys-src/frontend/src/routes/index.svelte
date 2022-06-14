ce4a31f0ec17da28e34a5931cf57fa550ca4fbd4
Stangl, Philipp <p.stangl1@oth-aw.de>
2022-06-14 20:02:04 +0200

    #12 update search component

--- sys-src/frontend/src/routes/index.svelte
+++ sys-src/frontend/src/routes/index.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
     import Charts from "$components/Charts.svelte";
     import Search from "$components/Search.svelte";
-    import Sample from "$components/Sample.svelte";
 </script>
 
 <main class="p-4 flex-grow">
     <Search/>
     <Charts/>
-    <Sample/>
 </main>
