b0264ce19490183718dab912a3237d2591f7319b
Weigl, Wolfgang <w.weigl@oth-aw.de>
2022-06-14 08:28:38 +0200

    Dockerfile angepasst strukurelle Änderungen

--- sys-src/reddit-crawler/src/crawler.ts
+++ sys-src/reddit-crawler/src/crawler.ts
@@ -1,5 +1,8 @@
-import { requester as crawler } from './snoowrap/snoowrap';
+import { requester as crawler } from './auth';
 import Snoowrap from 'snoowrap';
+
+// Backend connection
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const BackendURL = `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/graphql`;
 
@@ -30,41 +33,53 @@ async function transmitComment(comment: Snoowrap.Comment) {
     });
 }
 
-// setInterval(async () => {
-//     try {
-//         const result = await fetch(BackendURL, {
-//             method: 'post',
-//             body: JSON.stringify({
-//                 query: "query GetJobs { jobs }",
-//                 operationName: "GetJobs"
-//             }),
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//         });
+setInterval(async () => {
+    try {
+        const result = await fetch(BackendURL, {
+            method: 'post',
+            body: JSON.stringify({
+                query: "query GetJobs { jobs }",
+                operationName: "GetJobs"
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-//         const obj = await result.json();
-//         subredditsToSearch = (obj?.data?.jobs ?? []);
-//     }
-//     catch (ex: any) {
-//         console.log('error', ex);
-//     }
-// }, 60 * 1000);
+        const obj = await result.json();
+        subredditsToSearch = (obj?.data?.jobs ?? []);
+    }
+    catch (ex: any) {
+        console.log('error', ex);
+    }
+}, 60 * 1000);
 
 
+// Reddit Crawler
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // /**
 //  * Gets a Listing of new posts on this subreddit.
-//  * getNew.Options = {after?,before?,count?,limit?,show?}
 //  */
 crawler.getSubreddit('r/wallstreetbets').getNew({}).then(async (data) => {
-    let fetchedData = await data.fetchAll();
-    console.log(data.length);
-    console.log(fetchedData.length)
-    fetchedData.forEach(c => console.log(c.comments.toJSON()))
+    
+    let fetchedData = await data.fetchAll({
+        amount: 10,
+    });
 
-    // TODO: send to backend
+    // console.log(data.length);
+    // console.log(data.toJSON());
+    // console.log(fetchedData.length)
+
+    fetchedData.forEach(sraw => {
+        sraw.fetch().then(s => {
+            //console.log(s.comments.length); // ständig leer, weiß nicht warum
+            s.comments.forEach(c => {
+                // console.log(c.body);
+                transmitComment(c)
+            });
+        });
+    })
 })
 
 /**
@@ -72,32 +87,16 @@ crawler.getSubreddit('r/wallstreetbets').getNew({}).then(async (data) => {
  */
 crawler.getSubreddit('r/wallstreetbets').getNewComments({}).then(async (data) => {
 
+    const fetchedData = await data.fetchAll();
+
     // console.log(data.length);
-    // const fetchedData = await data.fetchAll();
     // console.log(fetchedData.length);
 
-    // fetchedData.forEach(c => transmitComment(c));
+    fetchedData.forEach(c => transmitComment(c));
 })
 
+
+setInterval(() => {
+    
+}, 60 * 1000);
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// var wallstreetbets : Snoowrap.Subreddit
-// setTimeout(() => {
-// crawler.getSubreddit('r/wallstreetbets').fetch().then(subreddit => {
-//         subreddit.refresh().then(refreshSubreddit =>{
-//             if(subreddit !== refreshSubreddit){
-//                 wallstreetbets = refreshSubreddit
-//                 console.log("SUBREDDIT REFRESHED");
-//                 console.log(wallstreetbets)
-//             }
-//             else {
-//                 console.log("SUBREDDIT NICHT REFRESHED");
-//             }
-//         })
-//     })
-// }, 0); // täglich = 86400000ms
-
-
-
-
