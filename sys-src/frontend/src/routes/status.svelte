25666757acfda8cfd69c267b3dee8d656458a10f
Stangl, Philipp <p.stangl1@oth-aw.de>
2022-06-20 12:45:16 +0200

    frontend query changes

---
+++ sys-src/frontend/src/routes/status.svelte
@@ -0,0 +1,66 @@
+<main class="p-4 flex-grow">
+
+    <div class="relative overflow-x-auto sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Komponente
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Letzte Verbindung
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                    Elastic Database
+                </th>
+                <td class="px-6 py-4">
+                    2022-06-07 09:03:07
+                </td>
+                <td class="px-6 py-4 text-green-600">
+                    Verfügbar
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                    Sentiment
+                </th>
+                <td class="px-6 py-4">
+                    2022-06-07 09:03:07
+                </td>
+                <td class="px-6 py-4 text-green-600">
+                    Verfügbar
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                    Stock Market Crawler
+                </th>
+                <td class="px-6 py-4">
+                    2022-06-07 09:03:07
+                </td>
+                <td class="px-6 py-4 text-green-600">
+                    Verfügbar
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                    Reddit Crawler
+                </th>
+                <td class="px-6 py-4">
+                    2022-06-07 09:03:07
+                </td>
+                <td class="px-6 py-4 text-red-600">
+                    Nicht verfügbar
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</main>
