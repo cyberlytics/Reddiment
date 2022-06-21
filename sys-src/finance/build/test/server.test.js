11c6fcdce7f1b611099d1f4e949be144f5c28556
Schweiger, Konrad <k.schweiger@oth-aw.de>
2022-06-21 10:24:42 +0200

    tests utils

--- sys-src/finance/build/test/server.test.js
+++ sys-src/finance/build/test/server.test.js
@@ -1,11 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = __importDefault(require("../src/server"));
-// test server to have route /posts
-it("should have route /post", () => {
-    const result = server_1.default.stack.find((layer) => layer.route && layer.route.path === "/post");
-    expect(result).toBeDefined();
-});
+// import router from "../src/server"
+// // test server to have route /posts
+// it("should have route /post", () => {
+//     const result = router.stack.find(
+//         (layer) => layer.route && layer.route.path === "/post")
+//     expect(result).toBeDefined()
+// });
