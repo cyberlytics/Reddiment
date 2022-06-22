868dbefbdc4a40b21301abed27ebab0efe972f26
Stangl, Philipp <p.stangl1@oth-aw.de>
2022-06-22 17:55:34 +0200

    refactor crawler and apply changes

---
+++ sys-src/reddit-crawler/src/backendConnection.ts
@@ -0,0 +1,74 @@
+/**
+ * Backend connection
+ */
+
+import Snoowrap from "snoowrap";
+import { requester as crawler } from "./auth";
+
+const BackendURL = `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/graphql`;
+
+export let subredditQueue = new Array<Snoowrap.Subreddit>();
+
+/**
+ * Send relevant comment data to backend
+ * @param comment a Snoowrap.Comment object, containing all comment data
+ */
+export async function transmitComment(comment: Snoowrap.Comment) {
+    try {
+        await fetch(BackendURL, {
+            method: 'post',
+            body: JSON.stringify({
+                query: "mutation AddComment($comment: Comment!) {addComment(comment: $comment)}",
+                variables: {
+                    comment: {
+                        articleId: comment.parent_id.substring(3),
+                        commentId: comment.id,
+                        subredditName: comment.subreddit_name_prefixed,
+                        text: comment.body,
+                        timestamp: new Date(comment.created_utc * 1000).toISOString(),
+                        userId: comment.author.id,
+                        upvotes: comment.ups,
+                        downvotes: comment.downs,
+                    }
+                },
+                operationName: "AddComment"
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+/**
+ * Receive a task list to work through in
+ */
+setInterval(async () => {
+    try {
+        const result = await fetch(BackendURL, {
+            method: 'post',
+            body: JSON.stringify({
+                query: "query GetJobs { jobs }",
+                operationName: "GetJobs"
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const obj = await result.json();
+        const subredditsToSearch = (obj?.data?.jobs ?? []);
+        for (const i in subredditsToSearch) {
+            const foundItems = subredditQueue.filter(
+                item => item.display_name === subredditsToSearch[i].slice(2)
+            );
+            if (foundItems.length !== 0) continue;
+            subredditQueue.push(crawler.getSubreddit(subredditsToSearch[i]));
+        }
+    }
+    catch (error: any) {
+        console.log('error', error);
+    }
+}, 10 * 1000);
+
--- sys-src/reddit-crawler/src/crawler.ts
+++ sys-src/reddit-crawler/src/crawler.ts
@@ -1,136 +1,41 @@
-import { requester as crawler } from './auth';
-import Snoowrap, { Comment, Listing, Submission, Subreddit } from 'snoowrap';
-
-// Backend connection
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-const BackendURL = `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/graphql`;
-
-let subredditsToSearch = new Array<string>();
-
 /**
- * Send relevant comment data to backend
- * @param comment a Snoowrap.Comment object, containing all comment data
- */
-async function transmitComment(comment: Snoowrap.Comment) {
-    try {
-        const result = await fetch(BackendURL, {
-            method: 'post',
-            body: JSON.stringify({
-                query: "mutation AddComment($comment: Comment!) {\n  addComment(comment: $comment)\n}",
-                variables: {
-                    comment: {
-                        articleId: comment.parent_id.substring(3),
-                        commentId: comment.id,
-                        subredditName: comment.subreddit_name_prefixed,
-                        text: comment.body,
-                        timestamp: new Date(comment.created_utc * 1000).toISOString(),
-                        userId: comment.author.id,
-                        upvotes: comment.ups,
-                        downvotes: comment.downs,
-                    }
-                },
-                operationName: "AddComment"
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-    } catch (error) {
-        console.log('error', error);
-    }
-}
-
-/**
- * Receive a task list to work through in 
- */
-setInterval(async () => {
-    try {
-        const result = await fetch(BackendURL, {
-            method: 'post',
-            body: JSON.stringify({
-                query: "query GetJobs { jobs }",
-                operationName: "GetJobs"
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        const obj = await result.json();
-        subredditsToSearch = (obj?.data?.jobs ?? []);
-    }
-    catch (error: any) {
-        console.log('error', error);
-    }
-}, 60 * 1000);
-
-
-/** 
  * Reddit Crawler
  */
-///////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const cs_limit = 100; 
-const sms_limit = 25;
+import { Subreddit } from "snoowrap";
+import { transmitComment, subredditQueue } from "./backendConnection";
 
-let sr_queue = new Array<Snoowrap.Subreddit>();
+const getRefreshedSubredditData = async (subreddit: Subreddit) => {
+    subreddit.refresh().then(async refreshedSubreddit => {
+        console.log(`${refreshedSubreddit.display_name} REFRESHED`);
 
-subredditsToSearch.push('r/wallstreetbets')
-subredditsToSearch.push('r/wallstreetbets')
+        // get new comments
+        const rawComments = await refreshedSubreddit.getNewComments();
 
-/**
- * Fill Subreddit Queue
- */
-setInterval(async () => {
-        try {
-            while(subredditsToSearch.length > 0){
-                let sr_name = subredditsToSearch.pop();
-                if(sr_name){
-                    let sr = crawler.getSubreddit(sr_name);
-                    sr_queue.push(sr);
-                    console.log("SUBREDDIT PUSHED");
-                    
-                }
-            }
-        } catch (error) {
-            console.log('error', error);
+        const fetchedComments = await rawComments.fetchMore({ amount: 100 });
+        for (let i = 0; i < fetchedComments.length; i++) {
+            await transmitComment(fetchedComments[i]);
         }
-}, 10 * 1000); // every 10 seconds
-
+    });
+}
 
 /**
  * Get all Comments of Subreddit and their Submissions
  */
 setInterval(async () => {
     try {
-        while (sr_queue.length > 0) {
-            let sr = sr_queue.pop();
-            if(sr){
-                await sr.refresh().then(rf_sr => {
-                    console.log("SUBREDDIT REFRESHED");
-                    
-                    rf_sr.getNewComments().then(async raw_cs => {
-                        let ft_cs = await raw_cs.fetchAll({amount: cs_limit});
-                        console.log("FETCHED COMMENTS: " + ft_cs.length);
-        
-                        ft_cs.forEach(c => transmitComment(c));
-                    })
-                    rf_sr.getNew().then(async raw_sms=> {
-                        const ft_sms = await raw_sms.fetchAll({amount: sms_limit});
-                        ft_sms.forEach(async raw_sm =>{
-                            await raw_sm.fetch().then(async ft_sm => {
-                                const ft_cs = await ft_sm.comments.fetchAll({amount: cs_limit});
-                                console.log("FETCHED COMMENTS IN SUBMISSION: " + ft_cs.length);
-                                
-                                ft_cs.forEach(c => transmitComment(c));
-                            })
-                        })
-                    })
-                })
-            }
+        const subreddit = subredditQueue.pop();
+        if (subreddit) {
+            await getRefreshedSubredditData(subreddit);
         }
+        /*
+        while (subredditQueue.length > 0) {
+            const subreddit = subredditQueue.pop();
+            if (subreddit) {
+                await getRefreshedSubredditData(subreddit);
+            }
+        }*/
     } catch (error) {
-        console.log('error', error);        
+        console.log('error', error);
     }
 }, 60 * 1000); // every 60 seconds
