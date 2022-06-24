845a1d7b921bdbda7281ae69a652cb8875b4130b
Stangl, Philipp <p.stangl1@oth-aw.de>
2022-06-24 08:43:02 +0200

    use database data in frontend

--- sys-src/frontend/src/lib/components/Charts.svelte
+++ sys-src/frontend/src/lib/components/Charts.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
     import Chart from "../shared/Chart.svelte";
-    import { sentiment } from "../mocks/sentimentMock";
     import { mentions } from "../mocks/mentionsMock";
     import { sentimentChart } from "../config/sentimentChart";
     import { mentionsChart} from "../config/mentionsChart";
     import { KQL_Stock } from "../graphql/_kitql/graphqlStores";
+    import { KQL_Subreddit } from "../graphql/_kitql/graphqlStores";
 
     let sentimentChartOptions = {
-        series: sentiment,
+        series: [
+            {
+                name: "Positiv",
+                data: $KQL_Subreddit.data?.subreddit?.sentiment.map(s => [Date.parse(s["time"]), s["positive"]]) ?? []
+            },
+            {
+                name: "Negativ",
+                data: $KQL_Subreddit.data?.subreddit?.sentiment.map(s => [Date.parse(s["time"]), s["negative"]]) ?? []
+            },
+            {
+                name: "Neutral",
+                data: $KQL_Subreddit.data?.subreddit?.sentiment.map(s => [Date.parse(s["time"]), s["neutral"]]) ?? []
+            },
+        ],
         ...sentimentChart
     };
 
@@ -15,9 +28,21 @@
         series: mentions,
         ...mentionsChart
     };
-</script>
 
-{JSON.stringify($KQL_Stock.data, null, 2)}
+    $: {
+        let myData = [
+            {
+                name: "Erwähnungen",
+                data: []
+            },
+            {
+                name: "Aktienpreis",
+                data: $KQL_Stock.data?.stock?.values?.map(v => [Date.parse(v["time"]), v["close"]]) ?? []
+            }
+        ]
+        console.log(myData)
+    }
+</script>
 
 <div class="grid grid-cols-2 gap-4">
     <div class="flex justify-center items-center bg-white rounded-lg m-4 pt-4 pb-2">
