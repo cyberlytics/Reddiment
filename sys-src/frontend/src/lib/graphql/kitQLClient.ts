1d58c8333794bd710d1204f9818bc4417f5c0bfc
Bauer, Tobias <t.bauer@oth-aw.de>
2022-06-24 20:40:02 +0200

    Fix hardcoded URL to backend

--- sys-src/frontend/src/lib/graphql/kitQLClient.ts
+++ sys-src/frontend/src/lib/graphql/kitQLClient.ts
@@ -1,7 +1,7 @@
 import { KitQLClient } from '@kitql/client';
 
 export const kitQLClient = new KitQLClient({
-    url: `http://localhost:4000/graphql`,
+    url: `/graphql`,
     headersContentType: 'application/json',
     logType: ['client', 'server', 'operationAndvariables']
 });
