e62d8783c1d2eaa0834a3314e7ea2fe0d09b0cde
Schweiger, Konrad <k.schweiger@oth-aw.de>
2022-06-27 20:22:25 +0200

    rm build dir

--- sys-src/finance-crawler/build/test/server.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import router from "../src/server"
-// // test server to have route /posts
-// it("should have route /post", () => {
-//     const result = router.stack.find(
-//         (layer) => layer.route && layer.route.path === "/post")
-//     expect(result).toBeDefined()
-// });
